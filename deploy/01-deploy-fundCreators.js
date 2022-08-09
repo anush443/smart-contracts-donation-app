ccc16ede29b394b2bc00fbc9f8ba02d3ad2b4109
anushgs <anush443@gmail.com>
2022-08-09 17:32:52 +0530

    minor

---
+++ deploy/01-deploy-fundCreators.js
@@ -0,0 +1,37 @@
+const { getNamedAccounts, deployments, network, run } = require("hardhat")
+const {
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+} = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    const arguments = [networkConfig[chainId]["minDonationAmount"]]
+
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+
+    const fundCreators = await deploy("FundCreators", {
+        from: deployer,
+        args: arguments,
+        log: true,
+        waitConfirmations: waitBlockConfirmations,
+    })
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(fundCreators.address, arguments)
+    }
+
+    const networkName = network.name == "hardhat" ? "localhost" : network.name
+    log(`yarn hardhat run scripts/enterRaffle.js --network ${networkName}`)
+    log("----------------------------------------------------")
+}
+module.exports.tags = ["all", "raffle"]
---
+++ deploy/02-update-front-end.js
@@ -0,0 +1,33 @@
+const fs = require("fs")
+const { network } = require("hardhat")
+
+const frontEndContractsFile = "../donationApp_client/src/constants/contractAddresses.json"
+const frontEndAbiFile = "../donationApp_client/src/constants/abi.json"
+
+module.exports = async () => {
+    if (process.env.UPDATE_FRONT_END) {
+        console.log("Writing to front end...")
+        updateContractAddresses()
+        updateAbi()
+        console.log("Front end written!")
+    }
+}
+
+async function updateAbi() {
+    const raffle = await ethers.getContract("FundCreators")
+    fs.writeFileSync(frontEndAbiFile, raffle.interface.format(ethers.utils.FormatTypes.json))
+}
+
+async function updateContractAddresses() {
+    const fundCreators = await ethers.getContract("FundCreators")
+    const contractAddresses = JSON.parse(fs.readFileSync(frontEndContractsFile, "utf8"))
+    if (network.config.chainId.toString() in contractAddresses) {
+        if (!contractAddresses[network.config.chainId.toString()].includes(fundCreators.address)) {
+            contractAddresses[network.config.chainId.toString()].push(fundCreators.address)
+        }
+    } else {
+        contractAddresses[network.config.chainId.toString()] = [fundCreators.address]
+    }
+    fs.writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses))
+}
+module.exports.tags = ["all", "frontend"]
